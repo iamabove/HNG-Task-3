7e713982b64d28c308eeb2a5a2347fd81977fe73
Emmanuel <emmanuellawson47@gmail.com>
2023-09-22 00:10:46 +0100

    second commit

--- hng-stage-3-task/src/App.jsx
+++ hng-stage-3-task/src/App.jsx
@@ -1,47 +1,13 @@
-import React, { useState } from "react";
+import React from 'react'
 import "./App.css";
-import Gallery from "./pages/Gallery";
-import { Search } from "./components/Search";
-import data from "./Data.json";
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Error from "./pages/Error";
-import Authentication from "./pages/Authentication";
-import PrivateRoutesLayout from "./Layouts/PrivateRoutesLayout";
+import Routespath from "./Routes/Routespath"
 
-function App() {
-  const [model, setModel] = useState(false);
-  const [tempImgSrc, setTempImgSrc] = useState("");
-
-  const getImage = (imgSrc) => {
-    setTempImgSrc(imgSrc);
-    setModel(true);
-  };
-
+const App = () => {
   return (
-    <div className="body">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/authentication" element={<Authentication />} />
-        <Route path="*" element={<Error />} />
-
-        <Route element={<PrivateRoutesLayout/>}>
-          <Route
-            path="/gallery"
-            element={
-              <Gallery
-                getImage={getImage}
-                model={model}
-                setModel={setModel}
-                tempImgSrc={tempImgSrc}
-                setTempImgSrc={setTempImgSrc}
-              />
-            }
-          />
-        </Route>
-      </Routes>
-    </div>
-  );
+    <>
+      <Routespath/>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ hng-stage-3-task/src/Routes/Routespath.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import Gallery from "../pages/Gallery";
+// import { Search } from "./components/Search";
+// import data from "./Data.json";
+import {
+  Routes,
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Home from "../pages/Home";
+import Error from "../pages/Error";
+import Authentication from "../pages/Authentication";
+import PrivateRoutesLayout from "../Layouts/PrivateRoutesLayout";
+
+function Routespath() {
+  const [model, setModel] = useState(false);
+  const [tempImgSrc, setTempImgSrc] = useState("");
+
+  const getImage = (imgSrc) => {
+    setTempImgSrc(imgSrc);
+    setModel(true);
+  };
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <div className="body">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/authentication" element={<Authentication />} />
+          <Route path="*" element={<Error />} />
+
+          <Route element={<PrivateRoutesLayout />}>
+            <Route
+              path="/gallery"
+              element={
+                <Gallery
+                  getImage={getImage}
+                  model={model}
+                  setModel={setModel}
+                  tempImgSrc={tempImgSrc}
+                  setTempImgSrc={setTempImgSrc}
+                />
+              }
+            />
+          </Route>
+        </Routes>
+      </div>
+    )
+  );
+  return <RouterProvider router={router}/>
+}
+
+export default Routespath;
